e8e9ffd0a23f0aae4e0eed55326266e7cdb72318
Priscilla Kusniaruk <priscilla.kusniaruk@outlook.com>
2020-07-30 23:15:16 -0300

    Ultimo

--- ProjetoFinal.VideoYoutube/src/projetofinal/videoyoutube/ProjetoFinalVideoYoutube.java
+++ ProjetoFinal.VideoYoutube/src/projetofinal/videoyoutube/ProjetoFinalVideoYoutube.java
@@ -1,5 +1,7 @@
 package projetofinal.videoyoutube;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Prisc
@@ -10,7 +12,7 @@ public class ProjetoFinalVideoYoutube {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Video v[] = new Video [3];
+      /*  Video v[] = new Video [3];
         v[0] = new Video("Aula 1 de POO");
         v[1] = new Video("Aula 12 de PHP");
         v[2] = new Video("Aula 10 de HTML5");
@@ -20,8 +22,21 @@ public class ProjetoFinalVideoYoutube {
         g[1] = new Gafanhoto("titi", "Pripri", 23, "M", 12036);
         
         
-        System.out.println(v[0].toString());
-        System.out.println(g[0].toString());
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[2]); //Rodrigo assiste HTML
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+        
+        //vis[1] = new Visualizacao(g[0], v[1]); // Pri assiste PHP
+        //vis[0].avaliar(35.0f);
+       // System.out.println(vis[0].toString());
+        
+        
+        
+        //System.out.println(v[0].toString());
+        //System.out.println(g[0].toString()); */
+
     }
+    
     
 }
